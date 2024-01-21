b4ef9a97fed7131269712ec1f5715c82238a06cf
zahra <z.hoseinpor98@gmail.com>
2024-01-21 16:48:37 +0330

    add js

---
+++ counter.js
@@ -0,0 +1,8 @@
+const decrementBtn = document.querySelector(".decrement");
+const incrementBtn = document.querySelector(".Increment");
+const resetBtn = document.querySelector(".reset");
+
+
+decrementBtn.addEventListener("click",()=>{});
+incrementBtn.addEventListener("click",()=>{});
+resetBtn.addEventListener("click",()=>{});
